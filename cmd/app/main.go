d0a73ff0f666ea68a674a9c4e4b7b171c4794028
PabstBlue1386 <longer1386@gmail.com>
2025-05-26 23:50:02 +0500

    In file main.go will be implemented test function FileExists

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/PabstBlue1386/BackupCarrier/internal/fileutils"
 )
 
 func main() {
-	fmt.Print("App running")
+	path := "C:\\soft\\hello.txt"
+	exists, err := fileutils.FileExists(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("File not found")
+		} else if os.IsPermission(err) {
+			fmt.Printf("No access")
+		}
+	}
+
+	if exists {
+		fmt.Printf("The file exists")
+	}
+
 }
